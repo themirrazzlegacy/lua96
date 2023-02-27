1a75f1a7b325b748512cf6d8147f3c4348651f19
themirrazz <89490659+themirrazz@users.noreply.github.com>
2023-02-27 08:28:11 -0800

    d

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 //!wrt $BSPEC:{"frn":"Lua96","aut":"themirrazz","dsc":"Run Lua programs on Windows 96","cpr":"Licensed under GNU GPL v3","ver":0.1,"ssy":"gui","icn":"C:/system/local/share/lua96/lua_logo.png"}
 const {Theme} = w96.ui;
-var terminal = this.boxedEnv.term;
+var term = this.boxedEnv.term;
 var term_instance;
-if(!terminal) {
+if(!term) {
     term_instance = await w96.util.requestTerminal();
-    terminal = term_instance.terminal;
+    term = term_instance.terminal;
 }
 class Lua96App extends WApplication {
     constructor() { super() }
     async main(argv) {
         if(!argv[0]) {
-            terminal.println("\x1b[31mNo file specified\x1b[0m");
+            term.println("\x1b[31mNo file specified\x1b[0m");
         }
         await term.pause();
     }
